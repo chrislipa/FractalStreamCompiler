edb89b5890c99665c4f4451fda9b102b1a1f5791
Chris <Chris@lipa>
2012-07-04 15:25:24 -0700

    delete build prodcts

--- build/FractalStreamCompiler.build/Debug/FractalStreamCompiler.build/Objects-normal/x86_64/FractalStreamCompiler.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompiler.cp \
-  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompiler.h \
-  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompilerPriv.h
