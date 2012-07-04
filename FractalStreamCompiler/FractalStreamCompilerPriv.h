ebc205395e3a2c2b0bac5c909847d56adf800cc2
Chris <Chris@lipa>
2012-07-04 15:19:47 -0700

    first

---
+++ FractalStreamCompiler/FractalStreamCompilerPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  FractalStreamCompilerPriv.h
+ *  FractalStreamCompiler
+ *
+ *  Created by Christopher Lipa on 7/4/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class FractalStreamCompilerPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
---
+++ build/FractalStreamCompiler.build/Debug/FractalStreamCompiler.build/Objects-normal/x86_64/FractalStreamCompiler.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompiler.cp \
+  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompiler.h \
+  /Users/lipa/fractalstream/external/FractalStreamCompiler/FractalStreamCompiler/FractalStreamCompilerPriv.h
