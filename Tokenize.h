267d52eb8061a1598de0e026ed93ee36d90a02aa
Chris <Chris@lipa>
2012-07-05 01:39:41 -0700

    aaaa

---
+++ Tokenize.h
@@ -0,0 +1,14 @@
+//
+//  Tokenize.h
+//  FractalStreamCompiler
+//
+//  Created by Christopher Lipa on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FractalStreamCompiler_Tokenize_h
+#define FractalStreamCompiler_Tokenize_h
+
+
+
+#endif
---
+++ language/build/tokens.h
@@ -0,0 +1,14 @@
+//
+//  tokens.h
+//  FractalStreamCompiler
+//
+//  Created by Christopher Lipa on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FractalStreamCompiler_tokens_h
+#define FractalStreamCompiler_tokens_h
+
+
+
+#endif
