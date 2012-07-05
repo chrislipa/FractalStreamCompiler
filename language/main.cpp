5f847300b6d859e122aa58e9b733c1cc61b53b04
Chris <Chris@lipa>
2012-07-04 20:19:54 -0700

    aaaa

--- language/main.cpp
+++ language/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "codegen.h"
+//#include "codegen.h"
 #include "node.h"
 
 using namespace std;
@@ -12,10 +12,10 @@ int main(int argc, char **argv)
 	yyparse();
 	std::cout << programBlock << endl;
     // see http://comments.gmane.org/gmane.comp.compilers.llvm.devel/33877
-	InitializeNativeTarget();
-	CodeGenContext context;
-	context.generateCode(*programBlock);
-	context.runCode();
+	//InitializeNativeTarget();
+	//CodeGenContext context;
+	//context.generateCode(*programBlock);
+	//context.runCode();
 	
 	return 0;
 }
