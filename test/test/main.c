9021cad73726cd66a50649b9c0e657b6b1b76b7c
Chris <Chris@lipa>
2012-07-05 08:17:25 -0700

    aaaa

---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Christopher Lipa on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
