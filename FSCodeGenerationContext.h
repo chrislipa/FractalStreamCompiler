afc25f35901cf751595f9c7f3a24a4493cf489a1
Chris <Chris@lipa>
2012-07-10 09:31:46 -0700

    more work

---
+++ FSCodeGenerationContext.h
@@ -0,0 +1,14 @@
+//
+//  FSCodeGenerationContext.h
+//  FractalStreamCompiler
+//
+//  Created by Christopher Lipa on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FractalStreamCompiler_FSCodeGenerationContext_h
+#define FractalStreamCompiler_FSCodeGenerationContext_h
+
+
+
+#endif
---
+++ language/node.cpp
@@ -0,0 +1,9 @@
+//
+//  node.cpp
+//  FractalStreamCompiler
+//
+//  Created by Christopher Lipa on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
